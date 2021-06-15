bbf81b0134052756240a5ce223655349195fd69f
Shinya Kumagai <roomful.rooms@gmail.com>
2021-06-15 17:57:14 +0900

    Introduce SwiftCoroutines
    - https://github.com/touchlab/SwiftCoroutines

---
+++ shared/src/iosMain/kotlin/com/example/shared/utils/SwiftCoroutines.kt
@@ -0,0 +1,88 @@
+package com.example.shared.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlin.native.concurrent.freeze
+
+// ref. https://github.com/touchlab/SwiftCoroutines
+
+class SuspendWrapper<T : Any>(
+    private val scope: CoroutineScope,
+    private val suspender: suspend () -> T
+) {
+    init {
+        freeze()
+    }
+
+    fun subscribe(
+        onSuccess: (item: T) -> Unit,
+        onThrow: (error: Throwable) -> Unit
+    ) = scope.launch {
+        try {
+            onSuccess(suspender().freeze())
+        } catch (error: Throwable) {
+            onThrow(error.freeze())
+        }
+    }.freeze()
+}
+
+class NullableSuspendWrapper<T>(
+    private val scope: CoroutineScope,
+    private val suspender: suspend () -> T
+) {
+    init {
+        freeze()
+    }
+
+    fun subscribe(
+        onSuccess: (item: T) -> Unit,
+        onThrow: (error: Throwable) -> Unit
+    ) = scope.launch {
+        try {
+            onSuccess(suspender().freeze())
+        } catch (error: Throwable) {
+            onThrow(error.freeze())
+        }
+    }.freeze()
+}
+
+class FlowWrapper<T : Any>(
+    private val scope: CoroutineScope,
+    private val flow: Flow<T>
+) {
+    init {
+        freeze()
+    }
+
+    fun subscribe(
+        onEach: (item: T) -> Unit,
+        onComplete: () -> Unit,
+        onThrow: (error: Throwable) -> Unit
+    ) = flow
+        .onEach { onEach(it.freeze()) }
+        .catch { onThrow(it.freeze()) } // catch{} before onCompletion{} or else completion hits rx first and ends stream
+        .onCompletion { onComplete() }
+        .launchIn(scope)
+        .freeze()
+}
+
+class NullableFlowWrapper<T>(
+    private val scope: CoroutineScope,
+    private val flow: Flow<T>
+) {
+    init {
+        freeze()
+    }
+
+    fun subscribe(
+        onEach: (item: T) -> Unit,
+        onComplete: () -> Unit,
+        onThrow: (error: Throwable) -> Unit
+    ) = flow
+        .onEach { onEach(it.freeze()) }
+        .catch { onThrow(it.freeze()) } // catch{} before onCompletion{} or else completion hits rx first and ends stream
+        .onCompletion { onComplete() }
+        .launchIn(scope)
+        .freeze()
+}
