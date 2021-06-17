d41d882c5ea5867f0b931587d289b3470f4626e6
Shinya Kumagai <roomful.rooms@gmail.com>
2021-06-17 18:35:50 +0900

    Get a user from github

--- shared/src/commonMain/kotlin/com/example/shared/data/source/remote/api/GitHubService.kt
+++ shared/src/commonMain/kotlin/com/example/shared/data/source/remote/api/GitHubService.kt
@@ -3,11 +3,42 @@ package com.example.shared.data.source.remote.api
 import com.chrynan.inject.Inject
 import com.chrynan.inject.Named
 import com.chrynan.inject.Singleton
+import com.example.shared.data.source.remote.api.response.UserResponse
+import com.github.droibit.komol.Komol
 import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.isSuccess
+import kotlin.coroutines.cancellation.CancellationException
 
 @Singleton
 class GitHubService @Inject constructor(
     @Named("githubApiBaseURL") private val baseURL: String,
     private val httpClient: HttpClient
 ) {
+    private val contentType = ContentType("application", "vnd.github.v3+json")
+
+    @Throws(ApiError::class, CancellationException::class)
+    suspend fun getUser(login: String): UserResponse {
+        val response: HttpResponse = httpClient.get("$baseURL/users/$login") {
+            accept(contentType)
+        }
+        return response.receiveIfSuccess()
+    }
+
+    //suspend fun getRepos(login: String)
+}
+
+private suspend inline fun <reified T> HttpResponse.receiveIfSuccess(): T {
+    try {
+        if (!status.isSuccess()) {
+            throw ApiError(this)
+        }
+        return receive()
+    } catch (e: Exception) {
+        throw ApiError(this, cause = e)
+    }
 }
---
+++ shared/src/commonMain/kotlin/com/example/shared/data/source/remote/api/response/UserResponse.kt
@@ -0,0 +1,22 @@
+package com.example.shared.data.source.remote.api.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * ref. https://docs.github.com/rest/reference/users#get-a-user
+ */
+@Serializable
+data class UserResponse(
+    val login: String,
+    val id: Int,
+    val url: String,
+    @SerialName("avatar_url") val avatarUrl: String,
+    @SerialName("repos_url") val reposUrl: String,
+    @SerialName("blog") val blogUrl: String?,
+    val name: String,
+    val company: String?,
+    val email: String?,
+    val followers: Int,
+    val following: Int
+)
