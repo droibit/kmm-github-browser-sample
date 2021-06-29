9b950154d447e73696a2eccd18fe1867f9045ae4
Shinya Kumagai <roomful.rooms@gmail.com>
2021-06-29 17:26:53 +0900

    iOS: Setup depndency graph

--- ios/Sources/DI/AppDelegate+Injection.swift
+++ ios/Sources/DI/AppDelegate+Injection.swift
@@ -1,6 +1,43 @@
 import Foundation
 import Resolver
+import Shared
 
 extension Resolver: ResolverRegistering {
-    public static func registerAllServices() {}
+    public static func registerAllServices() {
+        registerDataSources()
+        registerRepositories()
+    }
+
+    private static func registerDataSources() {
+        register { HttpClientProvider().get() }
+            .scope(.application)
+
+        register { GitHubService(httpClient: resolve()) }
+            .scope(.application)
+
+        register { AppDatabaseProvider().get() }
+            .scope(.application)
+    }
+
+    private static func registerRepositories() {
+        register {
+            UserRepositoryCombine(
+                delegate: UserRepositoryIos.Provider().get(
+                    gitHubService: resolve(),
+                    appDatabase: resolve()
+                )
+            )
+        }.implements(UserRepository.self)
+            .scope(.application)
+
+        register {
+            RepoRepositoryCombine(
+                delegate: RepoRepositoryIos.Provider().get(
+                    gitHubService: resolve(),
+                    appDatabase: resolve()
+                )
+            )
+        }.implements(RepoRepository.self)
+            .scope(.application)
+    }
 }
