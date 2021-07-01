9e9c2217430d1bd2ccd5bb00c97814201cc8b0be
Shinya Kumagai <roomful.rooms@gmail.com>
2021-07-01 19:31:49 +0900

    iOS: Add ContributorItemView

---
+++ ios/Sources/UI/Repo/Detail/Contributor/ContributorItemView.swift
@@ -0,0 +1,56 @@
+import Nuke
+import NukeUI
+import Shared
+import SwiftUI
+
+struct ContributorItemView: View {
+    let contributor: Contributor
+
+    var body: some View {
+        HStack(spacing: 8) {
+            LazyImage(source: contributor.avatarUrl) { state in
+                if let image = state.image {
+                    image
+                } else {
+                    SwiftUI.Image(systemName: "person.crop.circle.fill")
+                }
+            }
+            .processors([ImageProcessors.Resize(width: 32)])
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.secondary.opacity(0.25), lineWidth: 1))
+            .frame(width: 32, height: 32)
+
+            VStack(alignment: .leading, spacing: 4) {
+                Text(contributor.login)
+                    .font(.title3)
+                Text("\(contributor.contributions) commits")
+                    .font(.body)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding(.vertical, 8)
+        .frame(minHeight: 48)
+    }
+}
+
+struct ContributorItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        let contributor = Contributor(
+            repoName: "kmm-github-browser-sample",
+            repoOwner: "droibit",
+            login: "droibit",
+            contributions: 47,
+            avatarUrl: "https://avatars.githubusercontent.com/u/1456714?v=4"
+        )
+
+        ContributorItemView(contributor: contributor)
+            .background(Color(UIColor.systemBackground))
+            .previewLayout(.sizeThatFits)
+            .environment(\.colorScheme, .light)
+
+        ContributorItemView(contributor: contributor)
+            .background(Color(UIColor.systemBackground))
+            .previewLayout(.sizeThatFits)
+            .environment(\.colorScheme, .dark)
+    }
+}
