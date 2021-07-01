a03bba475401c8928408c6a73623876c4ecea7a5
Shinya Kumagai <roomful.rooms@gmail.com>
2021-07-01 19:32:14 +0900

    iOS: Add ContributorListView

---
+++ ios/Sources/UI/Repo/Detail/Contributor/Contributor+Identifiable.swift
@@ -0,0 +1,4 @@
+import Foundation
+import Shared
+
+extension Contributor: Identifiable {}
---
+++ ios/Sources/UI/Repo/Detail/Contributor/ContributorListView.swift
@@ -0,0 +1,47 @@
+import Shared
+import SwiftUI
+
+struct ContributorListView: View {
+    let contributors: [Contributor]
+
+    var body: some View {
+        if contributors.isEmpty {
+            EmptyView()
+        } else {
+            VStack(alignment: .leading) {
+                Text("Contributors")
+                    .font(.headline)
+                    .padding(.horizontal)
+                List(contributors) { contributor in
+                    ContributorItemView(contributor: contributor)
+                }
+                .listStyle(PlainListStyle())
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+    }
+}
+
+struct ContributorListView_Previews: PreviewProvider {
+    static var previews: some View {
+        let contributors = [
+            Contributor(
+                repoName: "kmm-github-browser-sample",
+                repoOwner: "droibit",
+                login: "droibit",
+                contributions: 47,
+                avatarUrl: "https://avatars.githubusercontent.com/u/1456714?v=4"
+            ),
+        ]
+
+        ContributorListView(contributors: contributors)
+            .background(Color(UIColor.systemBackground))
+            .previewLayout(.sizeThatFits)
+            .environment(\.colorScheme, .light)
+
+        ContributorListView(contributors: contributors)
+            .background(Color(UIColor.systemBackground))
+            .previewLayout(.sizeThatFits)
+            .environment(\.colorScheme, .dark)
+    }
+}
