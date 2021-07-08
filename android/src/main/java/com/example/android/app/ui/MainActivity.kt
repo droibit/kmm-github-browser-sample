3b1bad1f6ad21a12b5733c2c684fc5a7d4c3dcd5
Shinya Kumagai <roomful.rooms@gmail.com>
2021-07-08 16:38:53 +0900

    android: Initialization of Navigation Components

--- android/src/main/java/com/example/android/app/ui/MainActivity.kt
+++ android/src/main/java/com/example/android/app/ui/MainActivity.kt
@@ -5,16 +5,7 @@ import android.os.Bundle
 import com.example.shared.Greeting
 import android.widget.TextView
 import com.example.android.app.R
+import dagger.hilt.android.AndroidEntryPoint
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
-    }
-}
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
