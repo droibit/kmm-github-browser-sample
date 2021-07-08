1aa64f71e417825755aa76a9237a1b101204d33a
Shinya Kumagai <roomful.rooms@gmail.com>
2021-07-08 19:07:17 +0900

    android: Update SearchFragment layout

--- android/src/main/java/com/example/android/app/ui/search/SearchFragment.kt
+++ android/src/main/java/com/example/android/app/ui/search/SearchFragment.kt
@@ -1,6 +1,25 @@
 package com.example.android.app.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.android.app.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
