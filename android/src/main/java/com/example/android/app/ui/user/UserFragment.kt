310391dd3081aaab65efff102e1076122e2cf11b
Shinya Kumagai <roomful.rooms@gmail.com>
2021-07-12 18:14:13 +0900

    android: Update UserFragment layout

--- android/src/main/java/com/example/android/app/ui/user/UserFragment.kt
+++ android/src/main/java/com/example/android/app/ui/user/UserFragment.kt
@@ -1,11 +1,34 @@
 package com.example.android.app.ui.user
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.android.app.databinding.FragmentUserBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserFragment : Fragment() {
-
     private val args by navArgs<UserFragmentArgs>()
+
+    private var _binding: FragmentUserBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = viewLifecycleOwner
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
