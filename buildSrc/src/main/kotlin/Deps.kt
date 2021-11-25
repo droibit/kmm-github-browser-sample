c79dd9ce69a3173c6e49dd2532d7088ce47cb3cb
Shinya Kumagai <roomful.rooms@gmail.com>
2021-11-25 12:37:00 +0900

    android: Update jetpack deps

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -25,22 +25,22 @@ object Deps {
     }
 
     object Androidx {
-        const val core = "androidx.core:core-ktx:1.6.0"
-        const val activity = "androidx.activity:activity-ktx:1.3.0-rc01"
-        const val fragment = "androidx.fragment:fragment-ktx:1.4.0-alpha04"
-        const val appCompat = "androidx.appcompat:appcompat:1.4.0-alpha03"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
-        const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0"
+        const val core = "androidx.core:core-ktx:1.7.0"
+        const val activity = "androidx.activity:activity-ktx:1.4.0"
+        const val fragment = "androidx.fragment:fragment-ktx:1.4.0"
+        const val appCompat = "androidx.appcompat:appcompat:1.4.0"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.2"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.2.1"
 
         object Navigation {
-            internal const val version = "2.4.0-alpha04"
+            internal const val version = "2.4.0-beta02"
             const val runtime = "androidx.navigation:navigation-runtime-ktx:$version"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
         }
 
         object Lifecycle {
-            private const val version = "2.4.0-alpha02"
+            private const val version = "2.4.0"
             const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:$version"
             const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
